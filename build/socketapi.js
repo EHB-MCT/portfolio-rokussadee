5b0fe1a2310e7caee679fddbc55f8163bd45be2d
rokussadee <rokus.sadee@student.ehb.be>
2023-08-08 23:31:46 +0200

    feat(server-roles): server socket events for role creation
    on receiving 'set-role' emitter, server emits 'increment-rollcount' to all connected clients, and emits 'remove-rol' to all clients except the sender

--- build/socketapi.js
+++ build/socketapi.js
@@ -2,9 +2,9 @@ module.exports = function (io) {
   io.on('connection', (socket) => {
     console.log('A user connected');
     console.log(socket.id)
-  
+    
     // Handle socket events
-    socket.on('event-name', (data) => {
+    socket.on('event-name', data => {
       // Handle the event and broadcast to other clients
       socket.broadcast.emit('event-name', data);
     });
@@ -13,13 +13,16 @@ module.exports = function (io) {
       console.log('A user disconnected');
     });
     
-    socket.on('conect_error', (e) => {
+    socket.on('connect_error', (e) => {
       console.log(e) 
     })
 
-    socket.on('customevent', (number, string, obj) => {
-      console.log(number, string, obj)
+    socket.on('set-role', role => {
+      console.log("on set-role:",role )
+      io.emit('increment-rollcount')
+      socket.broadcast.emit('remove-role', role)
     })
+
 
     socket.on('error', (e) => {
       console.log(e)
