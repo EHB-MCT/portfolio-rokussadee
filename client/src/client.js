5ba272c7d36d3e28f1e9deac8c1b0a02b22b1b54
rokussadee <rokus.sadee@student.ehb.be>
2023-08-08 23:38:37 +0200

    feat(client-roles): client socket events for role creation
    dist/index.html:
    - html buttons for role selection
    
    src/client.js:
    - 'set-role' emits selected role to server
    - 'remove-role' receives role that is chosen by other client
    - 'increment-rollcount' updates number of roles that have been filles
    - setUserRole() takes chosen role as input and sets the clients
      'userRole' property to the given input
    - disableRole() handles html/js when roles are updated
    - nextPage() gets called when roleCount === 3, updates clients page
      based on their role.

--- client/src/client.js
+++ client/src/client.js
@@ -1,14 +1,73 @@
 import {io} from "socket.io-client";
 
 localStorage.debug = 'socket.io-client:socket'
-
-console.log("clientjs")
+console.log("clientjs loaded")
 
 const socket = io('http://localhost:3000')
 
-socket.on('connect', () => {
-  console.log(socket.id)
+let userRole;
 
-  socket.emit('customevent', 10, 'hi', {a: 'a'})
+let rollCount = 0;
+
+socket.on('connect', () => {
+  console.log(`Connected with id ${socket.id}`)
 })
 
+socket.on('remove-role', (role) => {
+  console.log("removed role:", role)
+  disableRole(role)
+})
+
+socket.on('increment-rollcount', () => {
+  rollCount += 1;
+  console.log(rollCount);
+  if(rollCount === 3) nextPage()
+})
+
+socket.on('next-page', () => {
+  console.log('next-page receive')
+  nextPage()
+})
+
+let roleButtons = document.querySelectorAll(".button")
+
+Array.prototype.forEach.call(roleButtons, element => {
+  console.log(element)
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+    const role = element.getAttribute("name")
+    console.log("client input value:", role)
+  
+    if(role === '') return
+  
+    socket.emit('set-role', role)
+    setUserRole(role)
+  })
+});
+
+function setUserRole(role) {
+  userRole = role;
+  disableRole(role)
+}
+
+
+function disableRole(role) {
+  console.log("client role:", role)
+  const roleButton = document.getElementById(`${role}`) 
+  roleButton.setAttribute("disabled", true)
+
+  if(userRole == role) {
+    console.log('styling role') 
+    roleButton.style.borderColor = "blue"
+    roleButton.style.color = "blue"
+  }
+}
+
+function nextPage() {
+  const container = document.getElementById("instrument-container")
+
+  const element = document.createElement("div")
+  element.textContent = userRole
+  container.appendChild(element)
+//  loadInstrument(userRole)
+}
