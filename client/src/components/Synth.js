f511e50c02fceec6c07043a65c1979d4a9eef4c0
rokussadee <rokus.sadee@student.ehb.be>
2023-08-19 19:54:07 +0200

    feat(client-synth-ui): piano ui complete

--- client/src/components/Synth.js
+++ client/src/components/Synth.js
@@ -1,29 +1,86 @@
-import React, {useState, useEffect, Fragment} from "react";
+import React, {useState, useEffect, Fragment, Component, useRef} from "react";
+import * as Tone from 'tone'
+import * as Nexus from 'nexusui'
+Nexus.context = Tone.getContext().rawContext._nativeAudioContext
 
-function Synth() {
-  const notes = {
-    "C4": 261.63,
-    "Db4": 277.18,
-    "D4": 293.66,
-    "Eb4": 311.13,
-    "E4": 329.63,
-    "F4": 349.23,
-    "Gb4": 369.99,
-    "G4": 392.00,
-    "Ab4": 415.30,
-    "A4": 440,
-    "Bb4": 466.16,
-    "B4": 493.88,
-    "C5": 523.25
+function Synth(){
+  const lowNoteRef = useRef(24);
+  const highNoteRef = useRef(60);
+  const pianoRef = useRef(null)
+
+  const keyMapper = { a: 0, w: 1, s: 2, e: 3, d: 4, f: 5, t: 6, g: 7, y: 8, h: 9, u: 10, j: 11, k: 12 }
+
+  const updatePiano = () => {
+    if (pianoRef.current) {
+      pianoRef.current.destroy();
+      createPiano(lowNoteRef.current, highNoteRef.current);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    const keyIndex = keyMapper[event.key]
+    keyIndex !== undefined && pianoRef.current && !pianoRef.current.keys[keyIndex]._state.state ?
+    pianoRef.current.toggleIndex(keyIndex, true) : null;
+
+    if (event.key === ',' && lowNoteRef.current >= 24) {
+      highNoteRef.current = lowNoteRef.current 
+      lowNoteRef.current -= 12
+      updatePiano();
+    }
+
+    if (event.key === '.' && highNoteRef.current <= 120) {
+      lowNoteRef.current = highNoteRef.current
+      highNoteRef.current += 12
+      updatePiano();
+    }
+
+  };
+  
+  const handleKeyUp = (event) => {
+    const keyIndex = keyMapper[event.key];
+    keyIndex !== undefined && pianoRef.current && pianoRef.current.keys[keyIndex]._state.state ? 
+    pianoRef.current.toggleIndex(keyIndex, false) : null;
+    
+  }
+
+  useEffect(() => {
+    createPiano(lowNoteRef.current, highNoteRef.current)
+    
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keyup', handleKeyUp)
+
+    return() => {
+      document.removeEventListener('keyup', handleKeyUp)
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  const synth = new Tone.Synth().toDestination()
+  
+  const playSynth = () => {
+    synth.triggerAttackRelease("C3", "16n");
+  }
+
+  const createPiano = (lowNote, highNote) => {
+    const piano = new Nexus.Piano('#piano',{
+      'size': [500,125],
+      'mode': 'button',  // 'button', 'toggle', or 'impulse'
+      'lowNote': lowNote,
+      'highNote': highNote
+    })
+
+    pianoRef.current = piano
+
   }
 
   return (
     <Fragment>
-      <ul>
-        {Object.entries(notes).map(([key, value]) => (
-          <li key={key} data-note={key}>{key}</li>
-        ))}
-      </ul>
+      <div>
+        <div id="piano" ></div>
+      </div>
+      <button onClick={()=>playSynth()}>
+      </button>
+
     </Fragment>
   )
 };
